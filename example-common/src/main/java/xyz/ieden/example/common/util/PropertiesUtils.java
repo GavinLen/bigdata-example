29a290bcc455cca8a22d3a168d4dffe0878085d8
gavinlen <gavin_len@163.com>
2019-09-02 07:41:57 +0800

    BigData init

---
+++ example-common/src/main/java/xyz/ieden/example/common/util/PropertiesUtils.java
@@ -0,0 +1,19 @@
+package xyz.ieden.example.common.util;
+
+import java.util.Properties;
+
+/**
+ * PropertiesUtils
+ *
+ * @author gavin
+ * @version 1.0
+ * @date 2019/9/2 7:23
+ */
+public class PropertiesUtils {
+
+    private static final String DEFAULT_CONFIG_ROOT_PATH = "classpath://";
+
+    public static Properties getProperties(String filePath) {
+        
+    }
+}
